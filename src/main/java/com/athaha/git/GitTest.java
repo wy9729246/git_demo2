2b35cb160283615439c5fdec5f497eefbfab472e
wanghh <1018875716@qq.com>
2022-07-13 16:28:34 +0800

    first commit

---
+++ src/main/java/com/athaha/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.athaha.git;
+
+/**
+ * @author WangHHH
+ * @version 1.0
+ * @className GitTest
+ * @description
+ * @date 2022/7/13 15:16
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
