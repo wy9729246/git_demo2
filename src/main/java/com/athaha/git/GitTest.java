19f9cc45aaffb7c3af81861874175838aa536692
wanghh <1018875716@qq.com>
2022-07-13 18:19:08 +0800

    hot-fix commit2

--- src/main/java/com/athaha/git/GitTest.java
+++ src/main/java/com/athaha/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello github");
         System.out.println("hello git3");
         System.out.println("git4");
+        System.out.println("hot-fix test");
     }
 }
