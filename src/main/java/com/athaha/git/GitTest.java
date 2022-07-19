67705027a21d54c5416b5503654604a192d56426
wanghh <1018875716@qq.com>
2022-07-19 16:39:55 +0800

    git2 commit

--- src/main/java/com/athaha/git/GitTest.java
+++ src/main/java/com/athaha/git/GitTest.java
@@ -19,7 +19,8 @@ public class GitTest {
         System.out.println("pull test");
         System.out.println("gitee test");
         System.out.println("gitee test2");
- System.out.println("git22 test2");
+        System.out.println("git22 test2");
+        System.out.println("1111111111111");
 
     }
 }
